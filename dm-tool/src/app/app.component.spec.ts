f110e3c778da4b5b26718c7d18597f51a8f1f531
Vajda Gergely <vajda.gergely@pillerkft.hu>
2024-12-28 17:39:48 +0100

    changed to standalone app

--- dm-tool/src/app/app.component.spec.ts
+++ dm-tool/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'dm-tool'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('dm-tool');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dm-tool');
-  });
 });
