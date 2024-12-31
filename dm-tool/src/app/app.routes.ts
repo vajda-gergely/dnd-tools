90e54279b7fbcbd8c97d04003ba8f7e7d32e966a
Vajda Gergely <vajda.gergely@pillerkft.hu>
2024-12-31 10:50:00 +0100

    rename modules to pages

--- dm-tool/src/app/app.routes.ts
+++ dm-tool/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'new-player',
-        loadComponent: () => import('./modules/new-player-help-guide/new-player-help-guide.component').then(m => m.NewPlayerHelpGuideComponent)
+        loadComponent: () => import('./pages/new-player-help-guide/new-player-help-guide.component').then(m => m.NewPlayerHelpGuideComponent)
     }
 ];
