43f8e533aaa4389af0953c49435f43c9522cd1ca
Vajda Gergely <vajda.gergely@pillerkft.hu>
2024-12-31 09:26:42 +0100

    added some partial components and css

--- dm-tool/src/app/app.component.ts
+++ dm-tool/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftMenuComponent } from "./components/left-menu/left-menu.component";
+import { TopMenuComponent } from "./components/top-menu/top-menu.component";
 
 @Component({
   selector: 'dmt-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenuComponent, LeftMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ dm-tool/src/app/components/left-menu/left-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dmt-left-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-menu.component.html',
+  styleUrl: './left-menu.component.scss'
+})
+export class LeftMenuComponent {
+
+}
